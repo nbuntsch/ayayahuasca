8b4f743f2ae596976ddab570676bd8714448f50c
nbuntsch <58217389+nbuntsch@users.noreply.github.com>
2023-06-23 23:45:46 +0200

    Install Vue3

---
+++ vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
