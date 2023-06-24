bc6cae6b75efd99424252a71f2092a30fd1101ea
nbuntsch <58217389+nbuntsch@users.noreply.github.com>
2023-06-24 18:10:55 +0200

    Display cards

---
+++ vue/src/components/CardItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :src="imgUrl" alt="" />
+</template>
+
+<script>
+export default {
+  props: {
+    card: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    imgUrl() {
+      return require(`@/assets/cards/${this.card.id}.png`);
+    },
+  },
+};
+</script>
--- vue/src/components/CardSelector.vue
+++ vue/src/components/CardSelector.vue
@@ -1,26 +1,36 @@
 <template>
   <h2>Card selector</h2>
-  <div v-for="card in randomCards" :key="card.id">
-    <div @click="play(card)">
-      <h3>{{ card.title }}</h3>
+  <div class="cards">
+    <div v-for="card in randomCards" :key="card.id">
+      <div @click="play(card)">
+        <card-item :card="card"></card-item>
+      </div>
     </div>
   </div>
+  <img :src="imgUrl" alt="" />
 </template>
 
 <script>
 import { collection, onSnapshot } from 'firebase/firestore';
 import { db } from '@/firebase/init.js';
 
+import CardItem from '@/components/CardItem.vue';
+
 export default {
+  components: {
+    CardItem,
+  },
   data: () => ({
     cards: [],
     randomCards: [],
+    imgUrl: '',
   }),
+
   mounted() {
     onSnapshot(collection(db, 'cards'), (snapshot) => {
       this.cards = [];
       snapshot.forEach((doc) => {
-        this.cards.push(doc.data());
+        this.cards.push({ ...{ id: doc.id }, ...doc.data() });
       });
       const shuffled = [...this.cards].sort(() => 0.5 - Math.random());
       this.randomCards = shuffled.slice(0, 3);
@@ -33,3 +43,9 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.cards {
+  display: flex;
+}
+</style>
