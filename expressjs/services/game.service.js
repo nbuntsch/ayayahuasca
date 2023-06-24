c0def15bfe000e0535baaa84d8d091bb9993c64a
nbuntsch <58217389+nbuntsch@users.noreply.github.com>
2023-06-24 03:17:28 +0200

    Get data in expressjs from firestore

--- expressjs/services/game.service.js
+++ expressjs/services/game.service.js
@@ -1,6 +1,28 @@
-const get = async (req, res) => {
-  const game = { gameId: 5678 };
+import 'dotenv/config';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+var game = [];
+
+onSnapshot(collection(db, 'games'), (snapshot) => {
+  snapshot.forEach((doc) => {
+    game = doc.data();
+  });
+});
+
+const get = async (req, res) => {
   return res.status(200).json(game);
 };
 
