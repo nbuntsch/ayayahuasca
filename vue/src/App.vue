fbd18a5e20e82694fe323144d567396fd46ce272
nbuntsch <58217389+nbuntsch@users.noreply.github.com>
2023-06-24 10:33:41 +0200

    Add fields to manage character state

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,69 @@
 <template>
+  <label>Overdose:</label>
+  <input type="text" v-model="overdose" />
+  <br />
+  <label>Mood:</label>
+  <input type="text" v-model="mood" />
+  <br />
+  <label>Excitement:</label>
+  <input type="text" v-model="excitement" />
+  <welcome-form :game="game"></welcome-form>
   <hello-world />
 </template>
 
 <script>
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { db } from '@/firebase/init.js';
+
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   components: {
     HelloWorld,
+    WelcomeForm,
+  },
+  data: () => ({
+    game: [],
+  }),
+  computed: {
+    overdose: {
+      get() {
+        return this.game.overdose;
+      },
+      set(value) {
+        this.game.overdose = value;
+        this.updateGame();
+      },
+    },
+    mood: {
+      get() {
+        return this.game.mood;
+      },
+      set(value) {
+        this.game.mood = value;
+        this.updateGame();
+      },
+    },
+    excitement: {
+      get() {
+        return this.game.excitement;
+      },
+      set(value) {
+        this.game.excitement = value;
+        this.updateGame();
+      },
+    },
+  },
+  mounted() {
+    onSnapshot(doc(db, 'games', 'game'), (snapshot) => {
+      this.game = snapshot.data();
+    });
+  },
+  methods: {
+    updateGame() {
+      const game = doc(db, 'games', 'game');
+      updateDoc(game, this.game);
+    },
   },
 };
 </script>
