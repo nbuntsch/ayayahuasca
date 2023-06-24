c43ed163a949145b4d39c60c1ac37facd9235869
nbuntsch <58217389+nbuntsch@users.noreply.github.com>
2023-06-24 11:33:26 +0200

    Reset game, login and start game

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,25 +7,44 @@
   <br />
   <label>Excitement:</label>
   <input type="text" v-model="excitement" />
-  <welcome-form :game="game"></welcome-form>
+  <br />
+  <button @click="resetGame">Reset game</button>
+  <button @click="startGame">Start game</button>
+  <welcome-form v-if="!playerId" :game="game" @login="login"></welcome-form>
+  <card-selector v-else></card-selector>
+  <div>player id: {{ playerId }}</div>
+  <div>player status: {{ playerStatus }}</div>
+  <div>activation code: {{ game.activationCode }}</div>
   <hello-world />
 </template>
 
 <script>
 import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '@/firebase/init.js';
+import { v4 as uuidv4 } from 'uuid';
 
 import HelloWorld from '@/components/HelloWorld.vue';
+import WelcomeForm from '@/components/WelcomeForm.vue';
+import CardSelector from '@/components/CardSelector.vue';
 
 export default {
   components: {
     HelloWorld,
     WelcomeForm,
+    CardSelector,
   },
   data: () => ({
     game: [],
+    playerId: null,
   }),
   computed: {
+    playerStatus() {
+      if (this.game.players && this.game.players[this.playerId]) {
+        return this.game.players[this.playerId].status;
+      } else {
+        return null;
+      }
+    },
     overdose: {
       get() {
         return this.game.overdose;
@@ -63,6 +82,28 @@ export default {
     updateGame() {
       const game = doc(db, 'games', 'game');
       updateDoc(game, this.game);
+    },
+    login() {
+      this.playerId = uuidv4();
+      this.game.players[this.playerId] = { status: 'idle' };
+      this.updateGame();
+    },
+    resetGame() {
+      this.game.players = {};
+      this.game.status = 'idle';
+      this.updateGame();
+    },
+    startGame() {
+      this.game.status = 'playing';
+      this.updateGame();
+      this.chooseFirstPlayer();
+    },
+    chooseFirstPlayer() {
+      const keys = Object.keys(this.game.players);
+      const firstPlayerId = keys[Math.floor(Math.random() * keys.length)];
+
+      this.game.players[firstPlayerId].status = 'playing';
+      this.updateGame();
     },
   },
 };
---
+++ vue/src/components/WelcomeForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Welcome</h1>
+  <input type="text" v-model="activationCode" />
+  <button @click="login">Play!</button>
+  <div>{{ game.players }}</div>
+</template>
+
+<script>
+export default {
+  emits: ['login'],
+  props: {
+    game: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    activationCode: '',
+  }),
+  methods: {
+    login() {
+      if (this.activationCode === this.game.activationCode) {
+        this.$emit('login');
+      } else {
+        alert("Mauvais code d'activation");
+      }
+    },
+  },
+};
+</script>
