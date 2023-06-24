e7b7ccccebd89e4d79c5ab380ad153631893f443
nbuntsch <58217389+nbuntsch@users.noreply.github.com>
2023-06-24 09:36:40 +0200

    Update data in firestore from Vue

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,25 +1,29 @@
 <template>
   <h1>Games:</h1>
-  <div>{{ games }}</div>
+  <div>{{ game }}</div>
+  <div @click="increment">increment</div>
 </template>
 
 <script>
-import { collection, onSnapshot } from 'firebase/firestore';
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '@/firebase/init.js';
 
 export default {
   data() {
     return {
-      games: [],
+      game: [],
     };
   },
   mounted() {
-    onSnapshot(collection(db, 'games'), (snapshot) => {
-      this.games = [];
-      snapshot.forEach((doc) => {
-        this.games.push(doc.data());
-      });
+    onSnapshot(doc(db, 'games', 'game'), (snapshot) => {
+      this.game = snapshot.data();
     });
+  },
+  methods: {
+    increment() {
+      const game = doc(db, 'games', 'game');
+      updateDoc(game, { test: this.game.test + 1 });
+    },
   },
 };
 </script>
